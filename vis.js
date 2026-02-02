b2d52f92e6cfbafeb4e409f0276fef8e6e579358
Alexander Krstovic <alexander.krstovic@gmail.com>
2026-02-01 23:21:21 -0800

    Added website structure and first pages

---
+++ vis.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  const svg = document.getElementById("art");
+
+  const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  circle.setAttribute("cx", 150);
+  circle.setAttribute("cy", 150);
+  circle.setAttribute("r", 80);
+  circle.setAttribute("fill", "#4CAF50");
+
+  svg.appendChild(circle);
+};
